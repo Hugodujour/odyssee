28286c1c3a791de72df7c3b35587c88c0107fbea
Hugo <hugogreg@live.fr>
2023-07-27 16:06:34 +0200

    la moula

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,8 @@
   "use strict";
 
 
-  
+  window.location.replace("http://www.vins-odyssee.com/fr");
+
 
 
     /**
@@ -226,7 +227,7 @@
   let vid = select('#bgvideo')
   if (vid) {
     const vidScrolled = () => {
-      window.scrollY > 600 ? vid.pause() : vid.play()
+      window.scrollY > 1 ? vid.play() : vid.play()
     }
     window.addEventListener('load', vidScrolled)
     onscroll(document, vidScrolled);
@@ -234,89 +235,13 @@
 
 
 
-  let genNew = select('#genNew')
-
-  const setBg = () => {
-    const randomColor = (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
-    const randomColor2 = (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
-    console.log(randomColor)
-    const root = document.querySelector(':root');
-    root.style.setProperty('--main-color', "#" + randomColor);
-    root.style.setProperty('--sec-color', "#" + randomColor2);
-    root.style.setProperty('--main-color-pro', "#" + randomColor);
-    root.style.setProperty('--sec-color-pro', "#" + randomColor2);
-    root.style.setProperty('--main-color-mer', "#" + randomColor);
-    root.style.setProperty('--sec-color-mer', "#" + randomColor2);
-    root.style.setProperty('--main-color-sau', "#" + randomColor);
-    root.style.setProperty('--sec-color-sau', "#" + randomColor2);
-  }
-  
-  genNew.addEventListener("click", setBg);
 
 
 
 
 
 
-  //carroussel
-// Variables to store the scroll position and rotation angle
-let scrollPosition = 0;
-let rotationAngle = 0;
-let shouldRotate = true; // Flag to control rotation
-
-// Add an event listener for the scroll event
-window.addEventListener('scroll', handleScroll);
-
-// Start the continuous rotation animation
-continuousRotation();
-
-// Function to handle the scroll event
-function handleScroll() {
-  // Update the scroll position
-  scrollPosition = window.scrollY;
-
-  // Update the rotation angle based on the scroll position
-  rotationAngle = scrollPosition * 0.2; // Adjust the multiplier to control the rotation speed
-
-  // Apply the rotation angle to the carousel element
-  const carousel = document.querySelector('.carousel');
-  carousel.style.transform = `rotateY(-${rotationAngle}deg)`;
-}
-
-// Function to continuously rotate the carousel
-function continuousRotation() {
-  if (shouldRotate) {
-    // Get the current rotation angle
-    const currentRotation = rotationAngle;
-
-    // Update the rotation angle for the continuous rotation
-    rotationAngle += 0.05; // Adjust the increment to control the continuous rotation speed
-
-    // Apply the rotation angle to the carousel element
-    const carousel = document.querySelector('.carousel');
-    carousel.style.transform = `rotateY(-${rotationAngle}deg)`;
-
-    // Schedule the next continuous rotation frame
-    requestAnimationFrame(continuousRotation);
-  }
-}
-
-// Add event listeners for hover on carousel
-const carousel = document.querySelector('.carousel');
-carousel.addEventListener('mousedown', stopCarouselRotation);
-carousel.addEventListener('mouseup', resumeCarouselRotation);
-
-// Function to stop the carousel rotation on hover
-function stopCarouselRotation() {
-  shouldRotate = false;
-}
-
-// Function to resume the carousel rotation when the mouse leaves
-function resumeCarouselRotation() {
-  shouldRotate = true;
-  continuousRotation();
-}
-
+ 
 
 
 
