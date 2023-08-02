435175ca1ca529971fae041454cdcb590e64889c
Hugo <hugogreg@live.fr>
2023-08-02 17:10:36 +0200

    hh

--- fr/assets/js/main.js
+++ fr/assets/js/main.js
@@ -251,7 +251,19 @@
     root.style.setProperty('--sec-color-sau', "#" + randomColor2);
   }
   
-  
+
+//boat
+const cursorImage = document.getElementById('cursor-image');
+const heroSection = document.getElementById('hero');
+
+heroSection.addEventListener('click', (event) => {
+  const xPos = event.clientX;
+  const yPos = event.clientY;
+
+  cursorImage.style.left = xPos + 'px';
+  cursorImage.style.top = yPos + 'px';
+  cursorImage.style.opacity = "1"
+});
 
 
 
