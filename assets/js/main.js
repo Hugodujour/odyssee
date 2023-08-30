b98a85faca6017cfb6d0b4dbe9db87ebad3220d2
Hugo <hugogreg@live.fr>
2023-08-30 17:09:42 +0200

    navigation mobile ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,20 +15,6 @@ heroSection.addEventListener('click', (event) => {
 });
 
 
-/**
-   * Easy event listener function
-   */
-const on = (type, el, listener, all = false) => {
-  let selectEl = select(el, all)
-  if (selectEl) {
-    if (all) {
-      selectEl.forEach(e => e.addEventListener(type, listener))
-    } else {
-      selectEl.addEventListener(type, listener)
-    }
-  }
-}
-
 
 })()
 
