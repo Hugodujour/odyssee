6fd68a49e8fb8116d3d79449b3c100cbaba92013
Hugo <hugogreg@live.fr>
2023-09-13 09:52:45 +0200

    animation navbar

--- fr/assets/js/new.js
+++ fr/assets/js/new.js
@@ -58,3 +58,10 @@ function resumeCarouselRotation() {
   shouldRotate = true;
   continuousRotation();
 }
+
+//mobile toggle classic
+const mobiled2 = document.getElementById('mobiled')
+const animed2 = document.getElementById('animed2')
+mobiled2.addEventListener('click', function () {
+ animed2.classList.toggle('animed2')
+})
--- fr/pages/join/new.js
+++ fr/pages/join/new.js
@@ -1,60 +1,6 @@
-console.log('new')
-
-  //carroussel
-// Variables to store the scroll position and rotation angle
-let scrollPosition = 0;
-let rotationAngle = 0;
-let shouldRotate = true; // Flag to control rotation
-
-// Add an event listener for the scroll event
-window.addEventListener('scroll', handleScroll);
-
-// Start the continuous rotation animation
-continuousRotation();
-
-// Function to handle the scroll event
-function handleScroll() {
-  // Update the scroll position
-  scrollPosition = window.scrollY;
-
-  // Update the rotation angle based on the scroll position
-  rotationAngle = scrollPosition * 0.2; // Adjust the multiplier to control the rotation speed
-
-  // Apply the rotation angle to the carousel element
-  const carousel = document.querySelector('.carousel');
-  carousel.style.transform = `rotateY(-${rotationAngle}deg)`;
-}
-
-// Function to continuously rotate the carousel
-function continuousRotation() {
-  if (shouldRotate) {
-    // Get the current rotation angle
-    const currentRotation = rotationAngle;
-
-    // Update the rotation angle for the continuous rotation
-    rotationAngle += 0.05; // Adjust the increment to control the continuous rotation speed
-
-    // Apply the rotation angle to the carousel element
-    const carousel = document.querySelector('.carousel');
-    carousel.style.transform = `rotateY(-${rotationAngle}deg)`;
-
-    // Schedule the next continuous rotation frame
-    requestAnimationFrame(continuousRotation);
-  }
-}
-
-// Add event listeners for hover on carousel
-const carousel = document.querySelector('.carousel');
-carousel.addEventListener('mousedown', stopCarouselRotation);
-carousel.addEventListener('mouseup', resumeCarouselRotation);
-
-// Function to stop the carousel rotation on hover
-function stopCarouselRotation() {
-  shouldRotate = false;
-}
-
-// Function to resume the carousel rotation when the mouse leaves
-function resumeCarouselRotation() {
-  shouldRotate = true;
-  continuousRotation();
-}
+//mobile toggle classic
+const mobiled2 = document.getElementById('mobiled')
+const animed2 = document.getElementById('animed2')
+mobiled2.addEventListener('click', function () {
+ animed2.classList.toggle('animed2')
+})
